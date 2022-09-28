fd0255524bca866cbe3fbcb9cf2cbb983c32ad00
robmcshinsky <robert.mcshinsky@gmail.com>
2022-09-28 16:50:51 -0600

    Started the server... got to 21.1.4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import About from "./components/About";
+import Home from "./pages/001_Home";
+import About from "./pages/002_About";
+import Navbar from "./components/001_Navbar";
+import Footer from "./components/100_Footer";
 
 function App() {
   return (
       <section>
-        <About></About>
+          <Navbar></Navbar>
+          <Home></Home>
+          <About></About>
+          <Footer></Footer>
       </section>
   );
 }
--- src/pages/002_About/index.js
+++ src/pages/002_About/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
     return (
-        <section>
-            <h1 id="about">Who am I?</h1>
-        </section>
+        <div>
+            <h1 id="about">About</h1>
+        </div>
     )
 }
 
