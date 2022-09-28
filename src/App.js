ea504a83b18f6f6103e331bf1e84e4dc50e577c9
Rob McShinsky <robmcshinsky@HBs-MacBook-Pro.local>
2022-09-28 10:22:33 -0600

    Created React App and first component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import About from "./components/About";
+
+function App() {
+  return (
+      <section>
+        <About></About>
+      </section>
+  );
+}
+
+export default App;
